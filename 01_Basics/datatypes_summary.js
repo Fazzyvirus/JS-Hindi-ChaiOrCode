04579e74cf9c2a360edbef7d0a7a1826cd5d70ed
Fazzyvirus <145501058+Fazzyvirus@users.noreply.github.com>
2024-03-27 20:38:25 +0000

    datatype

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -2,21 +2,21 @@
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
+// const score = 100
 // const score:Number = 100 - using in typescript
-const scoreValue = 100.3
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
 // to declare symbol
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
 // console.log(id === anotherId);
 
-const bigNumber = 4343432143435464343242324534n
+// const bigNumber = 4343432143435464343242324534n
 
 // console.log(bigNumber);
 
@@ -25,13 +25,41 @@ const bigNumber = 4343432143435464343242324534n
 
 // Arrays, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"]
+// const heros = ["shaktiman", "naagraj", "doga"]
 
-let myObj = {
-    name: "Faisal",
-    age: 22
-}
-const myFunction = function () {
-    console.log("hello world");
+// let myObj = {
+//     name: "Faisal",
+//     age: 22
+// }
+// const myFunction = function () {
+//     console.log("hello world");
+// }
+
+// ++++++++++++++++++++++++++++ Memory & Types ++++++++++++++++++++++++++
+
+// Stack (Primitive) => variable ka copy milta hai
+// Heap (Non-Primitive) => yaha se origional value ka reference milta h
+
+// let myYoutubeName = "hiteshchoudharydotcom"
+
+// let anotherName = myYoutubeName
+
+// console.log(anotherName);
+
+// anotherName = "chaiorcode"
+
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+// non primitive
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
