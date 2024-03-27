578267694228e40fe413b0e11cc0e0abc4885ec6
Fazzyvirus <145501058+Fazzyvirus@users.noreply.github.com>
2024-03-27 20:01:42 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0); // it will always give false at all the comparison
+
+console.log("2" == 2); // fine value is true
+console.log("2" === 2); // false as its strictly checking the data type also
