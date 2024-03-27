7a85b7d6f91bef1220b4d78fb7d9b9852ae82b6d
Fazzyvirus <145501058+Fazzyvirus@users.noreply.github.com>
2024-03-27 18:25:05 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = 33
+// console.log(typeof score);
+// let age = "40asa"
+// let valueInNumber = Number (age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// let test = null
+// let testInNumber = Number (test)
+// console.log(typeof testInNumber);
+// console.log(testInNumber);
+
+// let test1 = undefined
+// console.log(test1);
+// let testInNumber1 = Number (test1)
+// console.log(typeof testInNumber1);
+// console.log(testInNumber1);
+
+// "33" => 33
+// "40asa" => NaN
+// true => 1; false => 0
+// null => 0
+
+// let isLoggedIn = "faisal"
+
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "faisal" => true
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
